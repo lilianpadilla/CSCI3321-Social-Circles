2cfa13d366b8d08c73ad573fb97614305aab5ed5
Audrey Tollett <atollett@trinity.edu>
2025-03-24 22:24:50 -0500

    temp save

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var database = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'pink2PIG!',
+    password: 'WOAHpassword69!',
     database: 'social_circles'
   });
   
--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,29 @@ var router = express.Router();
 const db = require("../database/connection");
 
 router.get("/",(req,res) => {
+  console.log("home page activated");
+  res.render('index',{title:'Homepage'});
+});
+
+router.get("/characters",(req,res) => {
   console.log("all characters route");
   let sql = `SELECT * FROM characters;`;
   db.query(sql,(err,result) => {
       if(err) throw err;
       console.log(result);
-      res.render('index',{title:'Characters',characters:result});
+      res.render('characters',{title:'Characters',characters:result});
   })
   
 });
 
-router.get('/char_page/:charID', (req, res) => {
+router.get('/characters/:charID', (req, res) => {
   const characterID = req.params.charID;
-  console.log("char_page route");
+  console.log("characters route");
   let sql = `SELECT * FROM characters WHERE ID = ?;`;
   db.query(sql,[characterID],(err,result) =>{
     if(err) throw err;
     var row = result[0];
-    res.render('char_page',{title:'Character Page',character:row});
+    res.render('character_page',{title:'Character Page',character:row});
   });
 });
 
