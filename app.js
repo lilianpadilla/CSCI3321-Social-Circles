6717ebe7ae8eb95dd2046145107a714ae9ed5252
Molly Tyree <mollytyree@theblackpad.trinity.local>
2025-04-02 10:33:45 -0500

    added comment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var charactersRouter = require('./routes/characters');
 
 var app = express();
-
+//bee[p]boop
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
