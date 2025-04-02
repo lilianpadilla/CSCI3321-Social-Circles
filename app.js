14e1064a5f68e45a7672077d905f80160019ff09
Audrey T <atollett@trinity.edu>
2025-04-02 11:03:39 -0500

    incorrect username now doesnt crash site XD

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var charactersRouter = require('./routes/characters');
 
 var app = express();
+
+//session stuff
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, 
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000 // 1 day in milliseconds
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +42,7 @@ app._router.stack.forEach((r) => {
 app.use('/users', usersRouter);
 
 app.use(express.urlencoded({ extended: true }));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var mysql = require('mysql2');
 var router = express.Router();
 const db = require("../database/connection");
+var session = require('express-session');
 const hash = require('../js/cyber.js'); //will add password hash & checks when sign up page exists
+
+//function so people cant just type endpoints for user-only pages, e.g. user profile
+function isAuthenticated (req, res, next) {
+  if (req.session.user) next()
+  else next('route')
+}
 
 // Login page route
 router.get("/", (req, res) => {
@@ -15,23 +22,33 @@ router.post("/login", (req, res) => {
   const { username, password } = req.body;
   let sql = `SELECT * FROM users WHERE Username = ?;`;
   db.query(sql,[username], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    let dbpass = result[0].Password;
-    console.log(dbpass);
-    console.log(password === dbpass);
-    if (password === dbpass) { 
-      res.redirect("/home");
-    } else {
-      res.render("index", { title: "Login", error: "Invalid credentials" });
+    if (err){ 
+      throw err;
+    } else if (result.length === 0){ //Username not found case
+      res.render("index", { title: "Login", error: "Invalid credentials" })
+    }else{
+      console.log(result);
+      let dbpass = result[0].Password;
+      //console.log(dbpass);
+      console.log(password === dbpass);
+      if (password === dbpass) { 
+        req.session.userId = result[0].ID; // Store user ID in the session
+        req.session.username = result[0].Username; // Store username
+        res.redirect("/home");
+      } else { //Incorrect password case
+        res.render("index", { title: "Login", error: "Invalid credentials" });
+      }
     }
+    
   });
 });
 
 // Homepage route (defined outside the POST handler)
-router.get("/home", (req, res) => {
-  console.log("Homepage route activated");
-  res.render('home', { title: 'Homepage' });
+//isAuthenticated,
+//, user: req.session.username
+router.get("/home",  (req, res) => {
+    console.log("Homepage route activated");
+    res.render('home', { title: 'Homepage'});
 });
 
 
@@ -39,14 +56,14 @@ router.get("/userpage", (req, res) => {
   console.log("User profile page activated");
 
   const user = {
-    username: "admin", 
+    username: req.session.username, 
     email: "admin@example.com", 
     joinDate: "January 1, 2023"
   };
 
   res.render('userpage', {
     title: 'User Profile',
-    username: user.username,
+    username: req.session.username,
     email: user.email,
     joinDate: user.joinDate
   });
