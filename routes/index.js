db8949385afc44f4c309ec6f5412c7b51db5ad09
Audrey T <atollett@trinity.edu>
2025-04-07 21:42:44 -0500

    added change password

--- routes/index.js
+++ routes/index.js
@@ -200,6 +200,68 @@ router.get("/userpage", (req, res) => {
   res.render("index", { title: "Login", error: "Please log in to see user profile page." });
 });
 
+router.get('/change-password', isAuthenticated, (req,res) => {
+  console.log("User profile page attempt, redirected");
+  let sql = 'SELECT User_ID, Score, DATE_FORMAT(DatePlayed, "%M %d %Y") as DatePlayed FROM leaderboard WHERE User_ID = ? ORDER BY DatePlayed DESC;';
+  db.query(sql, [req.session.userId], (err, result) => {
+    if (err) throw err;
+    res.render('new_pass', {
+      title: 'User Profile',
+      username: req.session.username,
+      joinDate: req.session.joinDate,
+      scores: result
+    });
+  });
+});
+
+router.get('/change-password', (req,res) => {
+  console.log("User profile page activated");
+  res.render("index", { title: "Login", error: "Please log in to see user profile page." });
+});
+
+router.post("/changed-password", isAuthenticated, (req,res) =>{
+  const {old_password, new_password, repeat_new_password} = req.body;
+  console.log("password change page activated");
+  let sql = `SELECT ID, Password FROM users WHERE ID = ?;`;
+  db.query(sql,[req.session.userId], (err0, result) => { //get password from database
+    if (err0) throw err0;
+    bcrypt.compare(old_password, result[0].Password, function(err2, result2) { //compare database hashed to user inputted
+      if (err2) throw err2;
+      if(result2){ //correct old password case
+        if(new_password === repeat_new_password){ //matching new passwords case
+          bcrypt.hash(new_password, 5, function(err3, hash2) {
+            if(err3) throw err3;
+            let sql2 = `UPDATE users SET Password = ? WHERE ID = ?`;
+            db.query(sql,[hash2,req.session.userId], (err4,result) => {
+              if(err4) throw err4;
+              res.render("index", { title: "Login", error: "Password successfully changed. Please log in again." });
+            });
+          });
+        }else{ //not matching new passwords case
+          res.render('new_pass', {
+            title: 'User Profile',
+            username: req.session.username,
+            joinDate: req.session.joinDate,
+            error: "New Passwords don't match."
+          });
+        }
+      } else { //Incorrect old password case
+        res.render('new_pass', {
+          title: 'User Profile',
+          username: req.session.username,
+          joinDate: req.session.joinDate,
+          error: "Incorrect old password."
+        });
+      }
+    });
+  });
+});
+
+router.post("/changed-password",(req,res) =>{
+  console.log("User profile page activated");
+  res.render("index", { title: "Login", error: "Please log in to see user profile page." });
+});
+
 router.get("/characters", (req, res) => {
   console.log("all characters route");
   let sql = `SELECT * FROM characters;`;
